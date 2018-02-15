6d32aa148775180e22ba9fbeacfaf915f4058d1e
Watandeep Sekhon <watan.cp@gmail.com>
2018-02-14 19:28:56 -0500

    Bug fix & documentation

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,9 @@ const ref = db.ref('server/wagetracker');
 const timeEntryRef = ref.child('timeEntry');
 
 const push = ref => data => ref.push(data);
-const getOneByValue = ref => (valueKey, value) => ref.orderByChild(valueKey).limitToLast(1).once('value');
+
+//For a given reference, it will return the first record ordered by the give key
+const getOneByValue = ref => key => ref.orderByChild(key).limitToLast(1).once('value');
 
 const timeEntryPush = async data => {
   const currentUser = auth.currentUser;
@@ -32,20 +34,24 @@ const timeEntryPush = async data => {
   data.date_userID = data.date + data.userID;
 
   //Confirm that a data entry for this day doesn't already exist
-  let existingData = await getOneByValue(timeEntryRef)('date_userID', data.date_userID);
+  let existingData = await getOneByValue(timeEntryRef)('date_userID');
+  existingData = existingData.val();
 
-  //We get back key : { data... }
-  //By calling Object.values we get an array [0] : { data... }
-  existingData = Object.values(existingData.val())[0];
+  if (existingData) {
+    //We get back key : { data... }
+    //By calling Object.values we get an array [0] : { data... }
+    existingData = Object.values(existingData)[0];
+  }
 
   //Seems redundant but firebase returns the last record if it doesn't find a match.
   //Therefore, ensure the result actually contains the field data that we care about
   if (existingData && existingData.date_userID === data.date_userID) {
     throw new Error('Another record for today already exists');
   }
-  console.log('we done?');
-  await push(timeEntryRef)(data);
-  console.log('done!!!');
+
+  // console.log('we done?');
+  return push(timeEntryRef)(data);
+  // console.log('done!!!');
 };
 
 export { timeEntryPush };
