4c0ece3b32967d1ab82b8d06a2f1a879069bbd6f
Watandeep Sekhon <watan.cp@gmail.com>
2018-02-17 11:33:31 -0500

    More design changes

--- src/App.js
+++ src/App.js
@@ -26,22 +26,47 @@ class App extends Component {
 
   render() {
     const isLoggedIn = this.state.user;
+    if (!isLoggedIn) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Time Streak</h1>
+            <div className="App-intro">
+              <h3>An easy solution for individuals to log their daily working hours.</h3>
+            </div>
+          </header>
+          <div className="App-body">
+            <SignIn />
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hours tracker</h1>
-        </header>
-        <div className="App-body">
-          {isLoggedIn
-            ? <div>
-                <UserProfile />
-                <Dashboard />
+            <div className="features">
+              <div className="feature">
+                <h3>On-the-go time tracking</h3>
+                <div className="feature-description">
+                  Time Streak works seemlessly across all your devices allowing you to easily track your time anywhere,
+                  anytime.
+                </div>
               </div>
-            : <SignIn />}
+              <div className="feature">
+                <h3>Customizable timesheets</h3>
+                <div className="feature-description">
+                  Want to know how many hours you worked between two specific dates? No problem. Our flexible timesheet
+                  filters give users complete control over the timesheets.
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="App">
+          <div className="App-body">
+            <UserProfile />
+            <Dashboard />
+          </div>
+        </div>
+      );
+    }
   }
 }
 
